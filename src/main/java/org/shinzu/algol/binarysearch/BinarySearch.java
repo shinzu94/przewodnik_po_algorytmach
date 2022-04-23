ffc19adb09fa64fb44e4d0cca47bc527a7b88202
shinzu94 <oskar.sek94@gmail.com>
2022-04-23 20:32:41 +0200

    Feature/binary search (#2)
    Feature/binary search 
    * Dodatkowe testy i implementacja
    
    * Dodatkowe testy i poprawiona implementacja

--- src/main/java/org/shinzu/algol/binarysearch/BinarySearch.java
+++ src/main/java/org/shinzu/algol/binarysearch/BinarySearch.java
@@ -1,7 +1,31 @@
 package org.shinzu.algol.binarysearch;
 
 public class BinarySearch {
-    public static long find(int[] array, int search) {
-        return 0;
+    public static int findAny(int[] array, int search) {
+        int result = -1;
+        int length = array.length;
+
+        if (length > 0) {
+            int index = length / 2;
+            int begin = 0;
+            int end = length - 1;
+            int lastIndex = -1;
+            while (result == -1) {
+                if (array[index] > search) {
+                    end = index;
+                    lastIndex = index;
+                    index = index - ((end - begin + 1) / 2);
+                    if (index == lastIndex) break;
+                } else if (array[index] < search) {
+                    begin = index;
+                    lastIndex = index;
+                    index = index + ((end - begin) / 2);
+                    if (index == lastIndex) break;
+                } else {
+                    result = index;
+                }
+            }
+        }
+        return result;
     }
 }
--- src/test/java/org/shinzu/algol/binarysearch/BinarySearchTest.java
+++ src/test/java/org/shinzu/algol/binarysearch/BinarySearchTest.java
@@ -2,35 +2,89 @@ package org.shinzu.algol.binarysearch;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BinarySearchTest {
+    Random random = new Random();
+
     @Test
-    public void callFindForEmptyArrayShouldReturnMinusOne() {
-        long result = BinarySearch.find(new int[]{}, 1);
+    public void callFindAnyForEmptyArrayShouldReturnMinusOne() {
+        int result = BinarySearch.findAny(new int[]{}, 1);
         //Assert
         assertThat(result).isEqualTo(-1);
     }
 
     @Test
-    public void callFindForTwoElementsArrayAndSearchedFirstElementShouldFindFirstElement() {
+    public void callFindAnyForTwoElementsArrayAndSearchedFirstElementShouldFindFirstElement() {
         int firstElement = 1;
-        long result = BinarySearch.find(new int[]{firstElement, 5}, firstElement);
+        int result = BinarySearch.findAny(new int[]{firstElement, 5}, firstElement);
         //Assert
         assertThat(result).isEqualTo(0);
     }
 
     @Test
-    public void callFindForTwoElementsArrayAndSearchedSecondElementShouldFindSecondElement() {
+    public void callFindAnyForTwoElementsArrayAndSearchedSecondElementShouldFindSecondElement() {
         int secondElement = 5;
-        long result = BinarySearch.find(new int[]{1, secondElement}, secondElement);
+        int result = BinarySearch.findAny(new int[]{1, secondElement}, secondElement);
         //Assert
         assertThat(result).isEqualTo(1);
     }
 
     @Test
-    public void callFindForTwoElementsArrayWithoutSearchedValueShouldReturnMinusOne() {
-        long result = BinarySearch.find(new int[]{1, 5}, 2);
+    public void callFindAnyForTwoElementsArrayWithoutSearchedValueShouldReturnMinusOne() {
+        int result = BinarySearch.findAny(new int[]{1, 5}, 2);
+        //Assert
+        assertThat(result).isEqualTo(-1);
+    }
+
+    @Test
+    public void callFindAnyForTwentyFiveElementsArrayAndSearchedValueOfTwentyThreeElementShouldFindTwentyThreeElement() {
+        int length = 25;
+        int searchedIndex = 23;
+        int[] array = ThreadLocalRandom.current().ints(0, 50000).distinct().limit(length).sorted().toArray();
+
+        array = Arrays.stream(array).sorted().toArray();
+        int result = BinarySearch.findAny(array, array[searchedIndex]);
+        //Assert
+        assertThat(result).isEqualTo(searchedIndex);
+    }
+
+    @Test
+    public void callFindAnyForRandomLengthArrayAndSearchedValueOfRandomElementShouldFindTheRandomElement() {
+        int length = random.nextInt(100) + 1;
+        int searchedIndex = random.nextInt(length - 1);
+
+        int[] array = ThreadLocalRandom.current().ints(0, 50000).distinct().limit(length).sorted().toArray();
+
+        array = Arrays.stream(array).sorted().toArray();
+        int result = BinarySearch.findAny(array, array[searchedIndex]);
+        //Assert
+        assertThat(result).isEqualTo(searchedIndex);
+    }
+
+    @Test
+    public void callFindAnyForLargeArrayAndSearchedValueOfRandomElementShouldFindTheRandomElement() {
+        int length = 100_000;
+        int searchedIndex = random.nextInt(length - 1);
+        int[] array = ThreadLocalRandom.current().ints(-5_000_000, 5_000_000).distinct().limit(length).sorted().toArray();
+
+        int result = BinarySearch.findAny(array, array[searchedIndex]);
+        //Assert
+        assertThat(result).isEqualTo(searchedIndex);
+    }
+
+
+    @Test
+    public void callFindAnyForLargeArrayAndSearchedNotExistValueShouldReturnMinusOne() {
+        int length = 100_000;
+        int searchedIndex = random.nextInt(length - 1);
+        int[] array = ThreadLocalRandom.current().ints(-5_000_000, 5_000_000).distinct().limit(length).map(x -> x * 2).sorted().toArray();
+
+        int result = BinarySearch.findAny(array, array[searchedIndex] + 1);
         //Assert
         assertThat(result).isEqualTo(-1);
     }
