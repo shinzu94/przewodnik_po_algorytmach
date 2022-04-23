5de447814280ee02e90a858724910b9e2e34033a
Oskar Sęk <oskar.sek94@gmail.com>
2022-04-23 18:09:47 +0200

    Dodatkowe testy i implementacja

--- src/main/java/org/shinzu/algol/binarysearch/BinarySearch.java
+++ src/main/java/org/shinzu/algol/binarysearch/BinarySearch.java
@@ -1,7 +1,34 @@
 package org.shinzu.algol.binarysearch;
 
 public class BinarySearch {
-    public static long find(int[] array, int search) {
-        return 0;
+    public static int find(int[] array, int search) {
+        int result = -1;
+        int length = array.length;
+
+        if (length > 0) {
+            int lengthDiff = length/2;
+            int index = lengthDiff;
+            int lastLengthDiff = -1;
+            while (result == -1) {
+                if (array[index] > search) {
+                    index = index - lengthDiff;
+                    lengthDiff = lengthDiff/2;
+                    if (lengthDiff == 0 && lastLengthDiff == lengthDiff) {
+                        break;
+                    }
+                    lastLengthDiff = lengthDiff;
+                } else if (array[index] < search) {
+                    index = index - lengthDiff;
+                    lengthDiff = lengthDiff/2;
+                    if (lengthDiff == 0 && lastLengthDiff == lengthDiff) {
+                        break;
+                    }
+                    lastLengthDiff = lengthDiff;
+                } else {
+                    result = index;
+                }
+            }
+        }
+        return result;
     }
 }
--- src/test/java/org/shinzu/algol/binarysearch/BinarySearchTest.java
+++ src/test/java/org/shinzu/algol/binarysearch/BinarySearchTest.java
@@ -2,12 +2,17 @@ package org.shinzu.algol.binarysearch;
 
 import org.junit.Test;
 
+import java.util.Arrays;
+import java.util.Random;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class BinarySearchTest {
+    Random random = new Random();
+
     @Test
     public void callFindForEmptyArrayShouldReturnMinusOne() {
-        long result = BinarySearch.find(new int[]{}, 1);
+        int result = BinarySearch.find(new int[]{}, 1);
         //Assert
         assertThat(result).isEqualTo(-1);
     }
@@ -15,7 +20,7 @@ public class BinarySearchTest {
     @Test
     public void callFindForTwoElementsArrayAndSearchedFirstElementShouldFindFirstElement() {
         int firstElement = 1;
-        long result = BinarySearch.find(new int[]{firstElement, 5}, firstElement);
+        int result = BinarySearch.find(new int[]{firstElement, 5}, firstElement);
         //Assert
         assertThat(result).isEqualTo(0);
     }
@@ -23,15 +28,31 @@ public class BinarySearchTest {
     @Test
     public void callFindForTwoElementsArrayAndSearchedSecondElementShouldFindSecondElement() {
         int secondElement = 5;
-        long result = BinarySearch.find(new int[]{1, secondElement}, secondElement);
+        int result = BinarySearch.find(new int[]{1, secondElement}, secondElement);
         //Assert
         assertThat(result).isEqualTo(1);
     }
 
     @Test
     public void callFindForTwoElementsArrayWithoutSearchedValueShouldReturnMinusOne() {
-        long result = BinarySearch.find(new int[]{1, 5}, 2);
+        int result = BinarySearch.find(new int[]{1, 5}, 2);
         //Assert
         assertThat(result).isEqualTo(-1);
+    }
+
+
+    @Test
+    public void callFindForTwentyFiveElementsArrayAndSearchedValueOfTwentyThreeElementShouldFindSecondElement() {
+        int length = 25;
+        int searchedIndex = 23;
+        int[] array = new int[length];
+
+        for (int i = 0; i < length; i++) {
+            array[i] = random.nextInt(50000);
+        }
+        array = Arrays.stream(array).sorted().toArray();
+        int result = BinarySearch.find(array, array[searchedIndex]);
+        //Assert
+        assertThat(result).isEqualTo(1);
     }
 }
